7a00d2ce9b1c8c4387f35e1c47ae27729529e428
Austin Rupp <ruppelstiltskin@gmail.com>
2026-06-04 11:33:27 -0600

    added logo and header for frontpage

---
+++ header-front-page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Custom header for front-page.php
+ * Called via get_header( 'front-page' )
+ *
+ * @package Austin's_Theme
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'austins-theme' ); ?></a>
+
+<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-logo" rel="home">
+	<img
+		src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/logo.png"
+		alt="<?php bloginfo( 'name' ); ?>"
+		class="site-logo-img"
+	>
+</a>
--- header.php
+++ header.php
@@ -26,8 +26,16 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'austins-theme' ); ?></a>
 
 	<header id="masthead" class="site-header">
- 
-	<!-- 
+
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-logo" rel="home">
+			<img
+				src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/logo.png"
+				alt="<?php bloginfo( 'name' ); ?>"
+				class="site-logo-img"
+			>
+		</a>
+
+	<!--
     id="site-navigation" class="main-navigation navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
         <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
